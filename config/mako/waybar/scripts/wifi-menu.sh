1be6100d75a61de9cb4ca0fceb4dba45caf02baa
abhinav0411 <abhinavrai0419@gmail.com>
2025-08-23 02:00:52 +0530

    Added some more things

--- config/mako/waybar/scripts/wifi-menu.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# List available Wi-Fi networks
-chosen=$(nmcli -t -f SSID dev wifi | grep -v '^--' | sort -u | \
-    wofi --dmenu --prompt "Wi-Fi:" --lines=10)
-
-# If something was picked, connect
-[ -n "$chosen" ] && nmcli dev wifi connect "$chosen"
